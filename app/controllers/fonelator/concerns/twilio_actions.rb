6721c9202b90ec4f426c6889f0e4e3df7b4c1138
Brian Pattison <brian@brianpattison.com>
2014-02-20 11:28:51 -0600

    Add twilio outgoing xml

--- app/controllers/fonelator/concerns/twilio_actions.rb
+++ app/controllers/fonelator/concerns/twilio_actions.rb
@@ -1,20 +1,43 @@
 module Fonelator
   
   module TwilioActions
+    extend ActiveSupport::Concern
     
-    class AccountException < Exception
+    included do
+      before_filter :twilio_verify_sid
+      layout false
     end
     
-    def twilio_voice
-      if params[:AccountSid] == Fonelator::Config::twilio_account_sid
-        render layout: false
+    def twilio_voice_out
+      @outgoing_number = twilio_outgoing_number
+    end
+    
+  private
+  
+    def twilio_outgoing_number
+      # Use the number dialed or the number entered after being prompted
+      number = params[:Called].to_s.gsub(/\D/, "")
+      if number.blank? && params[:Digits]
+        number = params[:Digits].to_s.gsub(/\D/, "")
+      end
+      case number.length
+      when 12 # +17013334444 -> +17013334444
+        number
+      when 11 #  17013334444 -> +17013334444
+        "+#{number}"
+      when 10 #   7013334444 -> +17013334444
+        "+1#{number}"
+      when 7  #      3334444 -> +17013334444
+        "+1701#{number}"
       else
-        raise AccountException, "Account SID didn't match the configured account."
+        nil
       end
     end
-
-    def twilio_sms
-
+  
+    def twilio_verify_sid
+      if params[:AccountSid] != Fonelator::Config::twilio_account_sid
+        raise "Account SID didn't match the configured account."
+      end
     end
     
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Fonelator::Engine.routes.draw do
-  post 'webhooks/twilio_voice', to: 'webhooks#twilio_voice'
+  resources :webhooks do
+    collection do
+      post :twilio_sms_in
+      post :twilio_sms_out
+      post :twilio_voice_in
+      post :twilio_voice_out
+    end
+  end
 end
