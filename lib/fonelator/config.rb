0607e9d132b6b649299c77fdd8807dbd8d4e185b
Matthew Sullivan <msull92@gmail.com>
2014-02-25 10:26:32 -0600

    add default welcome message w/ configuration option, also change config var names

--- lib/fonelator/config.rb
+++ lib/fonelator/config.rb
@@ -3,8 +3,8 @@ module Fonelator
   class Config
   
     class << self
-      attr_accessor :default_country_code, :default_area_code, :default_number, :default_sip,
-                    :twilio_account_sid, :twilio_auth_token, :twilio_number, :user_class
+      attr_accessor :default_country_code, :default_area_code, :default_forwarding_number, :default_forwarding_sip,
+                    :twilio_account_sid, :twilio_auth_token, :twilio_number, :user_class, :welcome_message
     end
     
   end
